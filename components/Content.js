344476ea0f3495501eb359c71984ccb48417c0cc
Erik Behrends <behrends@users.noreply.github.com>
2020-10-02 12:06:27 +0200

    Text ist mit state selektierbar

--- components/Content.js
+++ components/Content.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Pressable, StyleSheet, Text } from 'react-native';
 
+// Alternativ können Function components mit Hooks state haben (useState)
 export default class Content extends React.Component {
+  // 1: initiale Zustand der Komponente (state)
+  state = { selected: false };
+
   render() {
+    let selectedStyle = { backgroundColor: 'orange' };
+    // 2: lesender Zugriff auf Eigenschaften des Zustands (this.state)
+    const currentState = this.state.selected;
+    if (currentState === false) selectedStyle = null;
+
+    // 3: State/Zustand wird verändert mit this.setState({....})
     return (
-      <Pressable onPress={() => alert('ok!!!!')}>
-        <Text style={styles.content}>
+      <Pressable
+        onPress={() => this.setState({ selected: !currentState })}
+      >
+        <Text style={[styles.content, selectedStyle]}>
           Inhalt der Notiz der auch sehr lang sein kann und über
           mehrere Zeilen geht
         </Text>
