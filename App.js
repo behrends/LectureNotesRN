6ff47509c64c9ac60b301a9afbb2dd5abc7dca44
Erik Behrends <behrends@users.noreply.github.com>
2020-10-02 09:15:36 +0200

    Kleine Anpassung

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>TIF18</Text>
+      <Text>TIF18A</Text>
       <StatusBar style="auto" />
     </View>
   );
