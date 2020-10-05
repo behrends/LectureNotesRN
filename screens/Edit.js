bc77c71c9abb76158410054a03f2e42140702397
Erik Behrends <behrends@users.noreply.github.com>
2020-10-05 14:03:53 +0200

    Titel der Notiz mit TextInput bearbeiten

--- screens/Edit.js
+++ screens/Edit.js
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Text, TextInput } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 
 export default class Edit extends React.Component {
+  state = { input: this.props.route.params.title };
+
   render() {
-    const route = this.props.route;
     return (
-      <Text style={{ marginTop: 100 }}>{route.params.title}</Text>
+      <View style={styles.container}>
+        <TextInput
+          style={styles.input}
+          value={this.state.input}
+          onChangeText={(text) => this.setState({ input: text })}
+        />
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  input: {
+    padding: 15,
+    borderWidth: 1,
+    width: '80%',
+  },
+});
