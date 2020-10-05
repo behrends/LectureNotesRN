ec423f0cc9a30bd2b544850f92051d881dc5a4dd
Erik Behrends <behrends@users.noreply.github.com>
2020-10-05 13:53:09 +0200

    Edit Screen als Klassenkomponente

--- screens/Edit.js
+++ screens/Edit.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, TextInput } from 'react-native';
 
-export default function Edit({ route }) {
-  return <Text style={{ marginTop: 100 }}>{route.params.title}</Text>;
+export default class Edit extends React.Component {
+  render() {
+    const route = this.props.route;
+    return (
+      <Text style={{ marginTop: 100 }}>{route.params.title}</Text>
+    );
+  }
 }
