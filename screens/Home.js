801f6a5c062ab2fa175048534c9c49aaa448a31a
Erik Behrends <behrends@users.noreply.github.com>
2020-10-05 12:11:00 +0200

    Navigation zu Details

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Button, StyleSheet, View } from 'react-native';
 
-export default function Home() {
+export default function Home({ navigation }) {
+  // const navigation = props.navigation; dies passiert in ({navigation})
   return (
     <View style={styles.container}>
-      <Button title="Notiz 1" onPress={() => alert('Notiz 1')} />
-      <Button title="Notiz 2" onPress={() => alert('Notiz 2')} />
+      <Button
+        title="Notiz 1"
+        onPress={() => navigation.navigate('Details')}
+      />
+      <Button
+        title="Notiz 2"
+        onPress={() => navigation.navigate('Details')}
+      />
     </View>
   );
 }
