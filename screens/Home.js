3bcf79c7bba90c7de06529e3f472d110edfe4910
Erik Behrends <behrends@users.noreply.github.com>
2020-10-05 11:54:06 +0200

    Home Screen mit Buttons für Notizen

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,19 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
 
 export default function Home() {
-  return <Text style={{ marginTop: 100 }}>HOME!</Text>;
+  return (
+    <View style={styles.container}>
+      <Button title="Notiz 1" onPress={() => alert('Notiz 1')} />
+      <Button title="Notiz 2" onPress={() => alert('Notiz 2')} />
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
