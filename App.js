5b30f5ae2beeceb35c6849ec80b6194ba9b917df
Erik Behrends <behrends@users.noreply.github.com>
2020-10-05 11:47:04 +0200

    Screens als eigene Dateien

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-// Home-Screen für die Liste der Notizen
-function Home() {
-  return <Text style={{ marginTop: 100 }}>HOME</Text>;
-}
-// Screen für Einstellungen
-function Settings() {
-  return <Text style={{ marginTop: 100 }}>SETTINGS</Text>;
-}
+import Home from './screens/Home';
+import Settings from './screens/Settings';
+import Details from './screens/Details';
 
 const Tab = createBottomTabNavigator();
 
@@ -24,13 +17,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    paddingTop: 60,
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-  },
-});
---
+++ screens/Home.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+export default function Home() {
+  return <Text style={{ marginTop: 100 }}>HOME!</Text>;
+}
