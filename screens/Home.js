8bea183ab6669434eb15fdbdce319c0974bd7343
Erik Behrends <behrends@users.noreply.github.com>
2020-10-05 15:35:54 +0200

    Cleanup

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, View } from 'react-native';
 import NoteListItem from '../components/NoteListItem';
 
 const noteList = [
