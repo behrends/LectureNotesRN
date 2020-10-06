8b501646f40d8b800b6e89cc0f4116cb9a0c1cf0
Erik Behrends <behrends@users.noreply.github.com>
2020-10-06 11:02:06 +0200

    Löschen für Notizen vorbereitet

--- components/NoteListItem.js
+++ components/NoteListItem.js
@@ -3,13 +3,20 @@ import { Pressable, StyleSheet, Text } from 'react-native';
 
 export default function NoteListItem(props) {
   return (
-    <Pressable onPress={props.onPress}>
+    <Pressable onPress={props.onPress} style={styles.container}>
       <Text style={styles.text}>{props.title}</Text>
+      <Pressable onPress={() => alert('delete!')}>
+        <Text>Löschen</Text>
+      </Pressable>
     </Pressable>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
   text: {
     fontSize: 40,
     fontWeight: '100',
