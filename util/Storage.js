2d5915292f6e897491a8639376623df34798073e
Erik Behrends <behrends@users.noreply.github.com>
2020-10-06 13:57:13 +0200

    ID wird als aktuelle Zeit in Millisekunden generiert

--- util/Storage.js
+++ util/Storage.js
@@ -3,9 +3,10 @@ import AsyncStorage from '@react-native-community/async-storage';
 export default class Storage {
   static async createNote(title) {
     const data = await AsyncStorage.getItem('notes');
-    const notes = JSON.parse(data);
-    // TODO eindeutige ID statt Array.length
-    notes.push({ id: notes.length + 1, title: title });
+    let notes = JSON.parse(data);
+    if (notes === null) notes = [];
+    const id = '' + Date.now(); // HACK: use now in milliseconds as ID
+    notes.push({ id: id, title: title });
     AsyncStorage.setItem('notes', JSON.stringify(notes));
     // SQLite ---> siehe Videokurs
   }
